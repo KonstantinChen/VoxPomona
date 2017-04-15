be26338b1d15868aeedf08710b17a480faf04698
lehuu711 <hil02013@mymail.pomona.edu>
2017-04-15 16:02:48 -0700

    -not working- modified urls.py in VoxPomona app folder

--- CS133Project/VoxPomona/urls.py
+++ CS133Project/VoxPomona/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+
+    #User Profile
+    #url(r'^create-user', views.create_user, name='create_user'),
+    #url(r'^$', views.user_profile, name='user_profile'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    #url(r'^profile', views.user_profile, name='user_profile'),
+    #url(r'^logout', views.logout_view, name='logout_view'),
+
 ]
