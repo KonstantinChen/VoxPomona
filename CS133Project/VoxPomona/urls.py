01abb96588eb3721549f49227357a76625067f24
lehuu711 <hil02013@mymail.pomona.edu>
2017-04-15 17:04:49 -0700

    added sign-up page via '/register', page loads with template

--- CS133Project/VoxPomona/urls.py
+++ CS133Project/VoxPomona/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     #User Profile
-    #url(r'^create-user', views.create_user, name='create_user'),
+    url(r'^register', views.sign_up, name='register'),
     #url(r'^$', views.user_profile, name='user_profile'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     #url(r'^profile', views.user_profile, name='user_profile'),
--- CS133Project/VoxPomona/views.py
+++ CS133Project/VoxPomona/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. I'm hungry")
+
+#User Creation
+def sign_up(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
