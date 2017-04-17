f66abcbfee45d661cc020710b31175d0b4ec0e5c
lehuu711 <hil02013@mymail.pomona.edu>
2017-04-16 20:23:00 -0700

    modifications to register_view function

--- CS133Project/VoxPomona/views.py
+++ CS133Project/VoxPomona/views.py
@@ -13,10 +13,11 @@ def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+        	user = User.objects.create_user(form.cleaned_data.get('email'),
+                form.cleaned_data.get('email'), form.cleaned_data.get('password'))
+            user.first_name = form.cleaned_data.get('first_name')
+            user.last_name = form.cleaned_data.get('last_name')
+            user.save()
             login(request, user)
             return redirect('home')
     else:
