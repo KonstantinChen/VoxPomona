1bb99bb4a7615447371ab2b14eb8ab7e52d5cbfc
lehuu711 <hil02013@mymail.pomona.edu>
2017-04-16 21:08:30 -0700

    reverted changes

--- CS133Project/VoxPomona/views.py
+++ CS133Project/VoxPomona/views.py
@@ -13,11 +13,10 @@ def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-        	user = User.objects.create_user(form.cleaned_data.get('email'),
-                form.cleaned_data.get('email'), form.cleaned_data.get('password'))
-            user.first_name = form.cleaned_data.get('first_name')
-            user.last_name = form.cleaned_data.get('last_name')
-            user.save()
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect('home')
     else:
