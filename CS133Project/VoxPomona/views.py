d0c6afe12731b3d341e98f23907a4bcda242739f
lehuu711 <hil02013@mymail.pomona.edu>
2017-04-20 20:15:41 -0700

    remove name splitting for default user (since we dont care about it)

--- CS133Project/VoxPomona/views.py
+++ CS133Project/VoxPomona/views.py
@@ -20,14 +20,6 @@ def register_view(request):
             # User default
             user = User.objects.create_user(form.cleaned_data.get('email'),
                 form.cleaned_data.get('email'), form.cleaned_data.get('password'))
-            name = form.cleaned_data.get('name')
-            nameL = name.split()
-            if (len(nameL) < 2):
-                user.first_name = "failed"
-                user.last_name = "name_creation"
-            else:
-                user.first_name = nameL[0]
-                user.last_name = nameL[1]
             user.save()
 
             #Create UserInfo to retain other info
