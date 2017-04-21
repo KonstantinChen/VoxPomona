7620683dfca2aad35a5ec57c1e28f92b4223dc11
lehuu711 <hil02013@mymail.pomona.edu>
2017-04-20 18:38:38 -0700

    added password input to register site -user info fields are still covered by webpage banner -need to get rid of double name input fields

--- CS133Project/VoxPomona/forms.py
+++ CS133Project/VoxPomona/forms.py
@@ -6,9 +6,26 @@ from VoxPomona.models import UserInfo
 
 
 class SignUpForm(forms.ModelForm):
-    email = forms.EmailField(max_length=254, help_text='Required field.')
-    full_name = forms.CharField(max_length=30, help_text='Required field.')
+    email = forms.CharField(label='email', max_length=500)
+    full_name = forms.CharField(label='name', max_length=128)
+
+    password = forms.CharField(label='password', max_length=128, widget=forms.PasswordInput)
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if len(email) > 29:
+            raise forms.ValidationError("Sorry, this email is too large.")
+        if email and User.objects.filter(email=email).count():
+            raise forms.ValidationError('Email addresses must be unique.')
+        return email
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        if len(password) <= 6:
+            raise forms.ValidationError('Password must be longer than 6 characters.')
+        return password
 
     class Meta:
         model = UserInfo
         fields = ('email', 'name', 'user_type')
+
