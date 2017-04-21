aad08e3384e2936623f52b001d6ea330ea9eacd1
Andi Chen <konstantinchen@gmail.com>
2017-04-20 20:05:38 -0700

    return full type name (?)

--- CS133Project/VoxPomona/models.py
+++ CS133Project/VoxPomona/models.py
@@ -17,7 +17,7 @@ class UserInfo(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name = "UserInfo", on_delete = models.CASCADE)
 
     def get_user_type(self):
-    	return self.user_type
+    	return USER_TYPE(self.user_type)
 
 
 class Petition(models.Model):
